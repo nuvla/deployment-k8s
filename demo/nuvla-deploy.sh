b721fabf4ee4560debddff16f3cd8bd046ec5e30
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2020-12-11 14:11:41 +0100

    demo deployment

---
+++ demo/nuvla-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#set -e
+
+kubectl create -f namespace.yaml
+
+kubectl -n nuvla-demo create -f zk-deployment.yaml
+
+kubectl -n nuvla-demo create -f es-deployment.yaml
+
+kubectl -n nuvla-demo create -f api-deployment.yaml
+
+kubectl -n nuvla-demo create -f jobs-deployment.yaml
+
+kubectl -n nuvla-demo create -f ui-deployment.yaml
+
+kubectl create -f traefik-rbac.yaml
+kubectl -n nuvla-demo create configmap traefik-conf --from-file=traefik.toml
+kubectl -n nuvla-demo create -f traefik-deployment.yaml
+kubectl -n nuvla-demo create -f traefik-ingress.yaml
